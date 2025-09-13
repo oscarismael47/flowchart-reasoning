78d60d954ee644f6e35d4445dd7460bbc8c81fe5
oscarismael47 <oscar.ismael.frausto.perez@gmail.com>
2025-09-12 21:29:01 -0600

    UPDATED: uml uploading

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import uuid
-from pathlib import Path
 import streamlit as st
 from streamlit_image_zoom import image_zoom
 from PIL import Image
@@ -10,6 +9,8 @@ from plantuml import PlantUML # https://github.com/vgilabert94/streamlit-image-z
 
 plantuml_server = PlantUML(url="http://www.plantuml.com/plantuml/img/") # Public PlantUML server (you can also host your own)
 out_folder = "out"
+diagram_path = f"{out_folder}/flow_chart.png"
+
 # Create folder if it does not exist
 if not os.path.exists(out_folder):
     os.makedirs(out_folder)
@@ -24,51 +25,66 @@ if "chat_id" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+
 if "image_path" not in st.session_state:
     st.session_state.image_path = None
-
-if "state_messages" not in st.session_state:
-    st.session_state.state_messages = []
 
 if "uml_code" not in st.session_state:
     st.session_state.uml_code = None
 
+if "chart_image_path" not in st.session_state:
+    st.session_state.chart_image_path = None
+
+
 with st.sidebar:
     
-            
     # Display chat messages from history on app rerun
     with st.container(height=400):
         for message in st.session_state.messages:
             with st.chat_message(name=message["role"]):
                 st.markdown(message["content"])
-                if "metadata" in message and "image" in message["metadata"]:
+                if "image" in message["metadata"]:
                     st.image(message["metadata"]["image"], width=400)
-                
-    #message = st.chat_input("What is up?"
+                if "uml_code" in message["metadata"]:
+                    with st.expander("PlantUML code"):
+                        st.code(message["metadata"]["uml_code"], language="plantuml")
+
     user_input = st.chat_input("What is up?",
                                 accept_file=True,
-                                file_type=["jpg", "jpeg", "png"],
+                                file_type=["jpg", "jpeg", "png", "puml"],
                             )
-    
-    if user_input:      
+
+
+    if user_input:     
+
         user_metadata = {}
         assistant_metadata = {}
 
+
         if  user_input.text:
             message = user_input.text
-            user_image_path = None
         
         if  user_input["files"]:
-            image_data = user_input["files"][0]
-            image_name = user_input["files"][0].name
-            user_metadata["image"] = image_data
-            user_image_path = f"{out_folder}/{image_name}"
-            with open(user_image_path, "wb") as f:
-                f.write(image_data.read())
+            if user_input["files"][0].type == "application/octet-stream":
+                stringio = StringIO(user_input["files"][0].getvalue().decode("utf-8"))
+                st.session_state.uml_code = stringio.read()
+                uml_code_name = user_input["files"][0].name
+                user_metadata["uml_code"] = st.session_state.uml_code
+                user_metadata["uml_code_name"] = uml_code_name
+
+
+            if user_input["files"][0].type in ["image/jpeg", "image/png"]:
+                image_data = user_input["files"][0]
+                image_name = user_input["files"][0].name
+                user_metadata["image"] = image_data
+                st.session_state.image_path = f"{out_folder}/{image_name}"
+                with open(st.session_state.image_path, "wb") as f:
+                    f.write(image_data.read())
+            
 
         response = invoke( message=message,
                            plantuml_code=st.session_state.uml_code,
-                           image_path=user_image_path,
+                           image_path=st.session_state.image_path,
                            thread_id=st.session_state.chat_id)
         
 
@@ -76,24 +92,11 @@ with st.sidebar:
         st.session_state.messages.append({"role": "assistant", "content": response, "metadata": assistant_metadata})
         st.rerun()
 
-    uploaded_file = st.file_uploader("Upload UML File")
-    if uploaded_file is not None:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.session_state.uml_code = stringio.read()
-
     if st.session_state.uml_code:
-        with open("diagram.png", "wb") as f:
+        with open(diagram_path, "wb") as f:
             f.write(plantuml_server.processes(st.session_state.uml_code))
-            st.session_state.image_path = "diagram.png"
+        st.session_state.chart_image_path = diagram_path
 
-        with st.expander("See PlantUML code"):
-            txt = st.text_area(
-                "Edit PlantUML code",
-                value=st.session_state.uml_code,
-                height=300,
-                label_visibility ="hidden",
-            )
-
-if st.session_state.image_path:
-    img = Image.open(st.session_state.image_path)
+if st.session_state.chart_image_path:
+    img = Image.open(st.session_state.chart_image_path)
     image_zoom(img, mode="both", size=(700, 500), keep_aspect_ratio=True, keep_resolution=True, zoom_factor=4.0, increment=0.2)
